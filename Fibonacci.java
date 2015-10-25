cac139f49823a936a7ea2223d2670d2302253c43
gielyn025 <gielyn025@gmail.com>
2015-10-25 17:19:54 +0800

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+import javac.util Scanner;
+
+public class Fibonacci{
+    int n;
+    int arr;
+    int array;
+
+  public static void main (String []args){
+
+     public int [] naiveFibonacci (int []arr,n){
+        (int []array = new int [n]);
+        array [0] = 0;
+        array [1] = 1;
+    for (int a=1; a<n; a++){
+
+        array [a]= array [a-1]+array [a-2];
+
+    }
+return array;
+     }
+  }
+
+}
